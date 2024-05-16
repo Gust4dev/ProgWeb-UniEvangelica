1dcbc4fcbcfd5f839b297529192960221a4b7817
Gustavo <gustavogomes034@outlook.com>
2024-05-16 17:54:39 -0300

    Feat: Adicionado campo de disciplina que tinha esquecido e comentarios no script

--- Atividade-2/script.js
+++ Atividade-2/script.js
@@ -1,20 +1,25 @@
-new Vue({
-  el: "#app",
+// * Curso de Engenharia de Software - UniEVANGÉLICA 
+// * Disciplina de Programação Web 
+// * Dev: Gustavo Gomes dos Santos - 2111267
+// * 16/05/2024 
+
+new Vue({ //start vue
+  el: "#app", //definir elemente que está sendo controlado pelo vue
   data: {
     newTask: "",
     tasks: [],
   },
-  methods: {
+  methods: { //methods aplicados a essa tarefa de TO-DO
     addTask: function () {
-      if (this.newTask.trim() !== "") {
+      if (this.newTask.trim() !== "") { //verifica se nao esta vazio
         this.tasks.push({ name: this.newTask, completed: false });
-        this.newTask = ""; // Limpa o input após adicionar a tarefa
+        this.newTask = ""; // limpa o input após adicionar a tarefa
       }
     },
-    completeTask: function (index) {
+    completeTask: function (index) { //method para concluida
       this.tasks[index].completed = true;
     },
-    removeTask: function (index) {
+    removeTask: function (index) { //method para excluir task
       this.tasks.splice(index, 1);
     },
   },
