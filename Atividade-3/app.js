121b401d96cf4af216c287273eaa9d3e831cbfac
Gustavo <gustavogomes034@outlook.com>
2024-05-23 14:38:38 -0300

    feat: added app.js books.js only with basics configs

---
+++ Atividade-3/app.js
@@ -0,0 +1,10 @@
+/*
+* Curso de Engenharia de Software - UniEVANGÉLICA 
+* Disciplina de Programação Web 
+* Dev: Gustavo Gomes dos Santos 
+* 23/05/2024
+*/ 
+
+const express = require('express');
+const app = express();
+const booksRouter = require('./routes/books')
